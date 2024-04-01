4a2f307b9ff7c7709ac2768758c8cb62f16dba9d
jo-millican <jmillic4@vols.utk.edu>
2024-04-01 16:29:09 -0400

    Improve R Code

--- profile.R
+++ profile.R
@@ -2,12 +2,18 @@ Rprof()
 start <- Sys.time()
 
 BDAY <- read.table("http://www.panix.com/~murphy/bdata.txt",head=TRUE) 
-BDAY <- BDAY[-367,] #take out 367th row, which contain totals for bdays\
+BDAY <- BDAY[-367,] #take out 367th row, which contain totals for bdays
 
 ntrials <- 500000
 counter <- 0
+
+bday <- sample(BDAY$date, size=ntrials*41, replace=TRUE, prob = BDAY$count)
+j <- 1
+
 for(i in 1:ntrials){
-  birthdays <- sample(BDAY$date, size=41, replace=TRUE, prob = BDAY$count)
+  birthdays <- bday[j:(j+40)]
+  j <- i+40
+  
   if(sum(duplicated(birthdays)) > 0){
     counter <- counter + 1
   }
